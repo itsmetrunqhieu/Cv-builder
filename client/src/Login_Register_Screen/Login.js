5457fefdd23aa740694b2daa27f4f5706cf627e9
dungtruong151 <dungtruong151@gmail.com>
2023-10-27 00:15:28 +0700

    finish login screen

--- client/src/Login_Register_Screen/Login.js
+++ client/src/Login_Register_Screen/Login.js
@@ -1,6 +1,17 @@
 import './Login.css';
+import React, { useState } from 'react';
 
 function Login() {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+
+  const imgStyle = {
+    cursor: 'pointer', // Đặt kiểu con trỏ khi di chuột vào hình ảnh thành "pointer"
+  };
+
   return (
     <div className="Login">
         <img 
@@ -25,15 +36,35 @@ function Login() {
             />
             <div className="Login-part">
                 <p className="Login-text">Login</p>
-                <div className="username-container">
-                  <input type="text" placeholder="Username" className="username"/>
+                <div className="username-form-field">
+                  <input type="text" className="form-input" placeholder=" "/>
+                  <label for="name" className="form-label">Username</label>
+                  <img 
+                      src='/Image/Login_Register_Screen/User_alt_light.svg'
+                      className='icon'
+                      alt='icon'
+                  />
                 </div>
-                <div className="password-container">
-                  <input type="password" placeholder="Password" className="username"/>
+                <div className="password-form-field">
+                  <input
+                    type={isPasswordVisible ? 'text' : 'password'}
+                    className="form-input"
+                    placeholder=" "
+                  />
+                  <label htmlFor="name" className="form-label">
+                    Password
+                  </label>
+                  <img
+                    src="/Image/Login_Register_Screen/View_hide_light.svg"
+                    className="icon"
+                    alt="icon"
+                    onClick={togglePasswordVisibility}
+                    style={imgStyle}
+                  />
                 </div>
-                <p className="forgot">Forgot password?</p>
+                <a href="/" className="forgot">Forgot password?</a>
                 <button className="login-button">Login</button>
-                <p className="dont-account">Don’t have account? <a href="/Register" style={{ color: 'blue' }}>Sign up</a></p>
+                <p className="dont-account">Don’t have account? <a href="/Register" style={{ color: '#62A4F0' }}>Sign up</a></p>
             </div>
         </main>
     </div>

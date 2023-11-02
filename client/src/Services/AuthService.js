467e4d75108b688fb7f7df8af5928342669323b7
meowie2k3 <dmhuy.ityu@gmail.com>
2023-11-02 22:51:58 +0700

    add a little register helper

--- client/src/Services/AuthService.js
+++ client/src/Services/AuthService.js
@@ -3,12 +3,12 @@ import Api from '@/services/Api'
 export default {
   // register endpoint
   register (credentials) {
-    /* do post request to /register endpoint on our backend */
-    return Api().post('register', credentials)
+    /* do post request to /signup endpoint on our backend */
+    return Api().post('auth/signup', credentials)
   },
   // login endpoint
   login (credentials) {
-    /* do post request to /login endpoint on our backend */
-    return Api().post('login', credentials)
+    /* do post request to /signin endpoint on our backend */
+    return Api().post('auth/signin', credentials)
   }
 }

f8df621d890300a0213ee8b63df5fb5237b23bea
meowie2k3 <dmhuy.ityu@gmail.com>
2023-11-03 00:22:11 +0700

    fix data lost

--- database/create database.sql
+++ database/create database.sql
@@ -1,1 +1,10 @@
 select * from dbo.Users
+
+create table Users
+(
+    id nvarchar(50) primary key not null,
+    Name nvarchar(50) not null,
+    Email nvarchar(50) not null,
+    Password nvarchar(50) not null,
+    role nvarchar(50) not null
+)

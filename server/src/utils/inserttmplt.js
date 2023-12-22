99473ee937bc86cff591be9ffe2bbebbe8f85a18
Mystic <thanhnhanjimmy@gmail.com>
2023-12-22 13:56:27 +0700

    Insert Template to Database Function as html string (input template path)

--- server/src/utils/inserttmplt.js
+++ server/src/utils/inserttmplt.js
@@ -4,7 +4,7 @@ const {cv_tmplt} = require('../models')
 const inserttmplt = (tmpltpath,previewdir) => {
     //read html file as a string
     fs.readFile(tmpltpath,'utf-8',async(err,htmlstr)=>{         
-        if (err) return err;
+        if (err) return console.error(err);
         //Find the exact template if exist(avoid duplicate). If not, insert html string to database
         [new_tmplt, created] = await cv_tmplt.findOrCreate({    
             where: {htmlstr},

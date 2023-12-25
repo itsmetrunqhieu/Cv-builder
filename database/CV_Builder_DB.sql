4d0f31500bac00bcbca1e828d692bb5f636d5d23
meowie2k3 <dmhuy.ityu@gmail.com>
2023-12-25 21:02:19 +0700

    DB sql file

---
+++ database/CV_Builder_DB.sql
@@ -0,0 +1,26 @@
+-- create CV_builder database
+CREATE DATABASE CV_builder;
+
+-- User table
+CREATE TABLE Users (
+    uid varchar(50) NOT NULL PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    email varchar(50) NOT NULL,
+    password varchar(72) NOT NULL,
+    role varchar(50) NOT NULL,
+    location varchar(50),
+    gender varchar(100),
+);
+-- CV table
+CREATE TABLE CVs (
+    uid varchar(50) NOT NULL PRIMARY KEY,
+    html_dir nvarchar(255) NOT NULL,
+    pdf_dir nvarchar(255) NOT NULL,
+ 
+);
+-- CV_tmplts table
+CREATE TABLE CV_tmplts (
+    uid varchar(50) NOT NULL PRIMARY KEY,
+	html_dir nvarchar(255) NOT NULL,
+	preview_dir nvarchar(255),
+);

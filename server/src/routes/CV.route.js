0d629d6d588a283d963fa096a60a9fe0a2ec6bba
trungquantrangnguyen <tranntquan@gmail.com>
2023-12-29 14:08:02 +0700

    fixed bug yayyy dmmmmmmmm

--- server/src/routes/CV.route.js
+++ server/src/routes/CV.route.js
@@ -4,7 +4,7 @@ const { verifyToken } = require("../utils/verifyToken");
 const CV_tmpltDTB = require("../middlewares/multerConfig");
 const router = express.Router();
 
-router.post("/download", verifyToken, CV_tmpltDTB.single("file"), downloadPDF);
-router.post("/saveUserCV/:fileName", verifyToken, saveUserCV);
+router.post("/download", CV_tmpltDTB.single("file"), downloadPDF);
+router.post("/saveUserCV/:fileName", saveUserCV);
 
 module.exports = router;

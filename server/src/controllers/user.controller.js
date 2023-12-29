64e4441fc681bd5e36c2536e3b8eeca7f7e09676
Mystic <thanhnhanjimmy@gmail.com>
2023-12-29 16:08:10 +0700

    Function: Update User Information

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,10 +1,36 @@
+const { User } = require("../models");
+
 const test = (req, res) => {
   res.json({
     message: "Hello World",
   });
 };
 
+const updateUser = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const { fullname, firstname, surname, phone, jobTitle, employer, citymunicipality, country } = req.body;
+    console.log (req.body);
+    const finduser = await User.findByPk(id);
+    if (!finduser) return res.status(404).json({msg: "User not found"});
+    finduser.set({
+      fullname: fullname,
+      firstname: firstname,
+      surname: surname,
+      phone: phone,
+      jobTitle: jobTitle,
+      employer: employer,
+      citymunicipality: citymunicipality,
+      country: country,
+    });
+    await finduser.save();
+    return res.status(200).json({msg: "User's information updated"});
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+}
 
-module.exports = {test};
+module.exports = {test, updateUser};
 //fsmegasale15
 //sieutuyet20

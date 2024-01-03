e28b5bd56cbacd5f7e2cc6adccddd2ac0cff3a0a
trann <trannquan@gmail.com>
2024-01-04 03:37:18 +0700

    add image to CV submit info

--- server/src/controllers/CVtmplt.controller.js
+++ server/src/controllers/CVtmplt.controller.js
@@ -1,4 +1,4 @@
-const { CV_tmplt } = require("../models");
+const { User, CV_tmplt } = require("../models");
 const fs = require("fs");
 const path = require("path");
 const errorHandler = require("../utils/error");
@@ -34,10 +34,10 @@ const deleteTmplt = async (req, res, next) => {
       "../../",
       `${deltmplt.html_dir}`
     );
-    fs.unlink(originalFilePath,async (err)=>{
+    fs.unlink(originalFilePath, async (err) => {
       if (err) console.error(err);
       await deltmplt.destroy();
-    })
+    });
     console.log("Template Deleted From Database");
     return res.status(200).json({ msg: "Template Deleted From Database" });
   } catch (error) {
@@ -90,9 +90,17 @@ const getTmplt = async (req, res, next) => {
   }
 };
 
-const submitInfor = (req, res, next) => {
+const submitInfor = async (req, res, next) => {
   try {
-    const data = req.body;
+    const findUser = await User.findByPk(req.user.id);
+    if (!findUser) return res.status(404).json({ msg: "User not found" });
+    const imageFilePath = path.join(
+      __dirname,
+      "../../",
+      findUser.profileImg_dir
+    );
+    const textData = req.body;
+    const data = { ...textData, profileImg_dir: imageFilePath };
     console.log(data);
     res.render(`${data.fileName}`, data);
   } catch (error) {
@@ -102,61 +110,66 @@ const submitInfor = (req, res, next) => {
 
 const updateTmpltPreview = async (req, res, next) => {
   try {
-    if (!req.file) return res.status(404).json({msg: "File not found"}); 
+    if (!req.file) return res.status(404).json({ msg: "File not found" });
     //get file path from multer
-    const filePath = path.join(__dirname,"../../",req.file.path);
+    const filePath = path.join(__dirname, "../../", req.file.path);
     //set preview template path destination
-    const Path = path.join("CV_tmpltpreviewDTB/",req.file.filename);
-    const previewPath = path.join(__dirname,"../../",Path);
+    const Path = path.join("CV_tmpltpreviewDTB/", req.file.filename);
+    const previewPath = path.join(__dirname, "../../", Path);
     //get template id
     const id = req.query.id;
-    console.log("Update CV preview ID: "+id);
+    console.log("Update CV preview ID: " + id);
     //find template with id
     const findtmplt = await CV_tmplt.findByPk(id);
-    if(!findtmplt) return res.status(404).json({msg: "Template not found"});
+    if (!findtmplt) return res.status(404).json({ msg: "Template not found" });
     //check if template already have preview
-    if(findtmplt.preview_dir){
+    if (findtmplt.preview_dir) {
       //delete old preview
       const originalFilePath = path.join(
         __dirname,
         "../../",
         `${findtmplt.preview_dir}`
       );
-      fs.unlink(originalFilePath,async (err)=>{
+      fs.unlink(originalFilePath, async (err) => {
         if (err) console.error(err);
       });
     }
     //copy preview file to preview folder and delete multer upload.
-    fs.copyFileSync(filePath,previewPath);
+    fs.copyFileSync(filePath, previewPath);
     fs.unlinkSync(filePath);
     //update preview directory in database
     findtmplt.preview_dir = Path;
     await findtmplt.save();
     console.log("Update Preview successfully");
-    return res.status(200).json({msg: "Template preview updated"});
+    return res.status(200).json({ msg: "Template preview updated" });
   } catch (err) {
     console.log(err);
     next(err);
   }
 };
 
-const getTmpltList = async (req, res, next) => { 
+const getTmpltList = async (req, res, next) => {
   try {
     //get template id and preview directory from database
-    const previewlist = await CV_tmplt.findAll({attributes: ['id','preview_dir']});
+    const previewlist = await CV_tmplt.findAll({
+      attributes: ["id", "preview_dir"],
+    });
     var List = [];
     //convert preview image to base 64
-    previewlist.forEach(preview =>{
-        base64preview = fs.readFileSync(path.join(__dirname,"../../",preview.preview_dir),'base64');
-        List.push(new Object({ID: preview.id,Base64: base64preview}));
-      })
-      //convert list to json and send.
+    previewlist.forEach((preview) => {
+      base64preview = fs.readFileSync(
+        path.join(__dirname, "../../", preview.preview_dir),
+        "base64"
+      );
+      List.push(new Object({ ID: preview.id, Base64: base64preview }));
+    });
+    //convert list to json and send.
     const listjson = JSON.stringify(List);
     return res.status(200).send(listjson);
   } catch (error) {
     next(error);
   }
-}
+};
 
 module.exports = {
   insertTmplt,
